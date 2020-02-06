353d4b8298b4fb19a659d69720488a0b1328773f
zfergus <zfergus@nyu.edu>
2020-02-06 15:51:56 -0500

    Changed project name to avoid conflicts

--- cmake/CCDWrapperDependencies.cmake
+++ cmake/CCDWrapperDependencies.cmake
@@ -8,7 +8,7 @@
 # Download and update 3rd_party libraries
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 list(REMOVE_DUPLICATES CMAKE_MODULE_PATH)
-include(CCDDownloadExternal)
+include(${PROJECT_NAME}DownloadExternal)
 
 ################################################################################
 # Required libraries
@@ -19,21 +19,21 @@ if(NOT TARGET Eigen3::Eigen)
   download_eigen()
   add_library(${PROJECT_NAME}_eigen INTERFACE)
   target_include_directories(${PROJECT_NAME}_eigen SYSTEM INTERFACE
-    $<BUILD_INTERFACE:${${PROJECT_NAME}_EXTERNAL}/eigen>
+    $<BUILD_INTERFACE:${CCD_WRAPPER_EXTERNAL}/eigen>
     $<INSTALL_INTERFACE:include>
   )
   set_property(TARGET ${PROJECT_NAME}_eigen PROPERTY EXPORT_NAME Eigen3::Eigen)
   add_library(Eigen3::Eigen ALIAS ${PROJECT_NAME}_eigen)
   # Set Eigen directory environment variable (needed for EVCTCD)
-  set(ENV{EIGEN3_INCLUDE_DIR} "${${PROJECT_NAME}_EXTERNAL}/eigen/")
+  set(ENV{EIGEN3_INCLUDE_DIR} "${CCD_WRAPPER_EXTERNAL}/eigen/")
 endif()
 
 # Etienne Vouga's CTCD Library
 if(NOT TARGET EVCTCD)
   download_evctcd()
-  add_subdirectory(${${PROJECT_NAME}_EXTERNAL}/EVCTCD)
+  add_subdirectory(${CCD_WRAPPER_EXTERNAL}/EVCTCD)
   # These includes are PRIVATE for some reason
-  target_include_directories(collisiondetection PUBLIC "${${PROJECT_NAME}_EXTERNAL}/EVCTCD/include")
+  target_include_directories(collisiondetection PUBLIC "${CCD_WRAPPER_EXTERNAL}/EVCTCD/include")
   # Turn of floating point contraction for CCD robustness
   target_compile_options(collisiondetection PUBLIC "-ffp-contract=off")
   # Rename for convenience
@@ -43,12 +43,12 @@ endif()
 # Brochu et al. [2012] and Tang et al. [2014]
 if(NOT TARGET exact-ccd::exact-ccd)
   download_exact_ccd()
-  add_subdirectory(${${PROJECT_NAME}_EXTERNAL}/exact-ccd EXCLUDE_FROM_ALL)
+  add_subdirectory(${CCD_WRAPPER_EXTERNAL}/exact-ccd EXCLUDE_FROM_ALL)
   add_library(exact-ccd::exact-ccd ALIAS exact-ccd)
 endif()
 
 # Rational implmentation of Brochu et al. [2012]
 if(NOT TARGET RationalCCD)
   download_rational_ccd()
-  add_subdirectory(${${PROJECT_NAME}_EXTERNAL}/rational_ccd)
+  add_subdirectory(${CCD_WRAPPER_EXTERNAL}/rational_ccd)
 endif()
--- cmake/CCDWrapperDownloadExternal.cmake
+++ cmake/CCDWrapperDownloadExternal.cmake
@@ -3,18 +3,18 @@ include(DownloadProject)
 # With CMake 3.8 and above, we can hide warnings about git being in a
 # detached head by passing an extra GIT_CONFIG option
 if(NOT (${CMAKE_VERSION} VERSION_LESS "3.8.0"))
-    set(CCD_EXTRA_OPTIONS "GIT_CONFIG advice.detachedHead=false")
+    set(CCD_WRAPPER_EXTRA_OPTIONS "GIT_CONFIG advice.detachedHead=false")
 else()
-    set(CCD_EXTRA_OPTIONS "")
+    set(CCD_WRAPPER_EXTRA_OPTIONS "")
 endif()
 
-function(ccd_download_project name)
+function(ccd_wrapper_download_project name)
     download_project(
         PROJ         ${name}
-        SOURCE_DIR   ${CCD_EXTERNAL}/${name}
-        DOWNLOAD_DIR ${CCD_EXTERNAL}/.cache/${name}
+        SOURCE_DIR   ${CCD_WRAPPER_EXTERNAL}/${name}
+        DOWNLOAD_DIR ${CCD_WRAPPER_EXTERNAL}/.cache/${name}
         QUIET
-        ${CCD_EXTRA_OPTIONS}
+        ${CCD_WRAPPER_EXTRA_OPTIONS}
         ${ARGN}
     )
 endfunction()
@@ -23,7 +23,7 @@ endfunction()
 
 # Eigen
 function(download_eigen)
-    ccd_download_project(eigen
+    ccd_wrapper_download_project(eigen
         URL           http://bitbucket.org/eigen/eigen/get/3.3.7.tar.bz2
         URL_MD5       05b1f7511c93980c385ebe11bd3c93fa
     )
@@ -31,7 +31,7 @@ endfunction()
 
 # Etienne Vouga's CTCD Library
 function(download_evctcd)
-  ccd_download_project(EVCTCD
+  ccd_wrapper_download_project(EVCTCD
     GIT_REPOSITORY https://github.com/evouga/collisiondetection.git
     GIT_TAG        e5fe5c9767207df5047e375fb20180a665ae186f
   )
@@ -39,7 +39,7 @@ endfunction()
 
 # exact-ccd (clone of Brochu et al. [2012] and Tang et al. [2014])
 function(download_exact_ccd)
-  ccd_download_project(exact-ccd
+  ccd_wrapper_download_project(exact-ccd
     GIT_REPOSITORY https://github.com/jiangzhongshi/exact-ccd.git
     GIT_TAG        305bb6f0e57d399b283161dc3669c260f90fb7f5
   )
@@ -47,7 +47,7 @@ endfunction()
 
 # Rational CCD (rational version of Brochu et al. [2012])
 function(download_rational_ccd)
-  ccd_download_project(rational_ccd
+  ccd_wrapper_download_project(rational_ccd
     GIT_REPOSITORY https://github.com/teseoch/Exact-CDD.git
     GIT_TAG        94b05129efa2c33348622b2890bd464cc95f65a7
   )
@@ -55,7 +55,7 @@ endfunction()
 
 # Catch2 for testing
 function(download_catch2)
-  ccd_download_project(Catch2
+  ccd_wrapper_download_project(Catch2
     GIT_REPOSITORY https://github.com/catchorg/Catch2.git
     GIT_TAG        v2.11.1
   )

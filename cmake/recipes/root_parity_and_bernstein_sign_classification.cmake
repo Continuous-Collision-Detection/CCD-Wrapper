81404e7c184c475b90ae21ccac833a7f0a2c731a
Zachary Ferguson <zfergus@nyu.edu>
2021-09-15 16:28:48 -0400

    Reduced RP and BSC folder name size

--- cmake/recipes/root_parity_and_bernstein_sign_classification.cmake
+++ cmake/recipes/root_parity_and_bernstein_sign_classification.cmake
@@ -7,12 +7,12 @@ message(STATUS "Third-party: creating target 'ccd_wrapper::root_parity_and_berns
 
 include(FetchContent)
 FetchContent_Declare(
-    root_parity_and_bernstein_sign_classification
+    rp_and_bsc
     GIT_REPOSITORY https://github.com/Continuous-Collision-Detection/Root-Parity-and-Bernstein-Sign-Classification.git
     GIT_TAG 1686f79ca4b3c95979e2f6a96504d4562e44d3ce
     GIT_SHALLOW FALSE
 )
 
-FetchContent_MakeAvailable(root_parity_and_bernstein_sign_classification)
+FetchContent_MakeAvailable(rp_and_bsc)
 
 add_library(ccd_wrapper::root_parity_and_bernstein_sign_classification ALIAS exact-ccd)
